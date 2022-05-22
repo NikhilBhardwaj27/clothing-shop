9543a1e74b6b70d64184edb1a14918524ba72a72
Nikhil Bhardwaj <sharmanikhil65613@gmail.com>
2022-05-22 23:40:58 +0530

    redux and redux-saga completed

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import App from './App';
+import { store, persistor } from './store/store';
+
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
